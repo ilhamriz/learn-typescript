1f10b44470cdd9053a194df718e0247ed6c008a8
ilhamriz <milhamrizky7@gmail.com>
2023-01-02 21:20:22 +0700

    lesson-9

--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,14 @@
-console.log("hello");
+type stringOrNum = string | number;
+type objWithName = { name: string, uid: stringOrNum };
+
+const logDetails = (uid: stringOrNum, item: string) => {
+  console.log(`${item} has a uid of ${uid}`);
+}
+
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+}
+
+const greetAgain = (user: objWithName) => {
+  console.log(`${user.name} says hello`);
+}
