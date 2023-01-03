91b6bba4bb9ca57e7f881afe513ef64cb75a73b3
ilhamriz <milhamrizky7@gmail.com>
2023-01-03 09:44:13 +0700

    lesson-11

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+// const anchor = document.querySelector('a')!;
+// if (anchor) console.log(anchor.href);
+// console.log(anchor.href)
+
+// const form = document.querySelector('form')!;
+const form = document.querySelector('form') as HTMLFormElement;
+// console.log(form.children);
+
+// INPUTS
+const type = document.querySelector("#type") as HTMLSelectElement;
+const toFrom = document.querySelector("#type") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(
+    type.value,
+    toFrom.value,
+    details.value,
+    amount.valueAsNumber,
+  )
+})
